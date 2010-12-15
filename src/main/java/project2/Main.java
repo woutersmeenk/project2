44e504acf597757c31c7b27b3c9e1a36b0a632d2
Wouter Smeenk <woutersmeenk@gmail.com>
2010-12-15 11:44:43 +0100

    level loading

--- src/main/java/project2/Main.java
+++ src/main/java/project2/Main.java
@@ -23,11 +23,12 @@ package project2;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
+import project2.level.XMLLevelLoader;
+import project2.model.level.Level;
 import project2.util.JavaLoggingToCommonLoggingRedirector;
 import triggers.Condition;
 import triggers.ConditionAnd;
 import triggers.DebugResponse;
-import triggers.Response;
 import triggers.Trigger;
 import triggers.TriggerManager;
 
@@ -35,12 +36,9 @@ import com.jme3.app.SimpleApplication;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
 import com.jme3.math.Vector3f;
-import com.jme3.renderer.Camera;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.Node;
-import com.jme3.scene.Spatial;
 import com.jme3.scene.shape.Box;
-import com.sun.org.apache.xpath.internal.operations.And;
 
 public class Main extends SimpleApplication {
     private static final Log LOG = LogFactory.getLog(Main.class);
@@ -48,70 +46,71 @@ public class Main extends SimpleApplication {
     TriggerManager triggerManager;
 
     public static void main(final String[] args) {
-	JavaLoggingToCommonLoggingRedirector.activate();
-	final Main app = new Main();
-	app.start();
+        JavaLoggingToCommonLoggingRedirector.activate();
+        XMLLevelLoader loader = new XMLLevelLoader();
+        Level level = loader.loadLevel(ClassLoader.getSystemResource("simpel.xml"));
+        final Main app = new Main();
+        app.start();
     }
 
     public Main() {
-	triggerManager = new TriggerManager();
+        triggerManager = new TriggerManager();
     }
 
     @Override
     public void update() {
-	// update game logic
-	triggerManager.update();
+        // update game logic
+        triggerManager.update();
 
-	super.update();
+        super.update();
     }
 
     @Override
     public void simpleInitApp() {
 
-	// create a blue box at coordinates (1,-1,1)
-	final Box box1 = new Box(new Vector3f(1, -1, 1), 1, 1, 1);
-	final Geometry blue = new Geometry("Box", box1);
-	final Material mat1 = new Material(assetManager,
-		"Common/MatDefs/Misc/SolidColor.j3md");
-	mat1.setColor("m_Color", ColorRGBA.Blue);
-	blue.setMaterial(mat1);
+        // create a blue box at coordinates (1,-1,1)
+        final Box box1 = new Box(new Vector3f(1, -1, 1), 1, 1, 1);
+        final Geometry blue = new Geometry("Box", box1);
+        final Material mat1 = new Material(assetManager,
+                "Common/MatDefs/Misc/SolidColor.j3md");
+        mat1.setColor("m_Color", ColorRGBA.Blue);
+        blue.setMaterial(mat1);
 
-	// create a red box straight above the blue one at (1,3,1)
-	final Box box2 = new Box(new Vector3f(1, 3, 1), 1, 1, 1);
-	final Geometry red = new Geometry("Box", box2);
-	final Material mat2 = new Material(assetManager,
-		"Common/MatDefs/Misc/SolidColor.j3md");
-	mat2.setColor("m_Color", ColorRGBA.Red);
-	red.setMaterial(mat2);
+        // create a red box straight above the blue one at (1,3,1)
+        final Box box2 = new Box(new Vector3f(1, 3, 1), 1, 1, 1);
+        final Geometry red = new Geometry("Box", box2);
+        final Material mat2 = new Material(assetManager,
+                "Common/MatDefs/Misc/SolidColor.j3md");
+        mat2.setColor("m_Color", ColorRGBA.Red);
+        red.setMaterial(mat2);
 
-	// create a pivot node at (0,0,0) and attach it to root
-	final Node pivot = new Node("pivot");
-	rootNode.attachChild(pivot);
+        // create a pivot node at (0,0,0) and attach it to root
+        final Node pivot = new Node("pivot");
+        rootNode.attachChild(pivot);
 
-	// attach the two boxes to the *pivot* node!
-	pivot.attachChild(blue);
-	pivot.attachChild(red);
-	// rotate pivot node: Both boxes have rotated!
-	pivot.rotate(0.4f, 0.4f, 0.0f);
+        // attach the two boxes to the *pivot* node!
+        pivot.attachChild(blue);
+        pivot.attachChild(red);
+        // rotate pivot node: Both boxes have rotated!
+        pivot.rotate(0.4f, 0.4f, 0.0f);
 
-	// create a custom condition
-	Condition condition = new ConditionAnd(
-		new Condition() {
+        // create a custom condition
+        Condition condition = new ConditionAnd(new Condition() {
 
-		    @Override
-		    public boolean isTrue() {
-			return getCamera().getDirection().x > 0;
-		    }
-		}, new Condition() {
+            @Override
+            public boolean isTrue() {
+                return getCamera().getDirection().x > 0;
+            }
+        }, new Condition() {
 
-		    @Override
-		    public boolean isTrue() {
-			return getCamera().getDirection().y > 0;
-		    }
-		});
+            @Override
+            public boolean isTrue() {
+                return getCamera().getDirection().y > 0;
+            }
+        });
 
-	triggerManager.addTrigger(new Trigger(condition, new DebugResponse(
-		"You are lookin the the positive x and y direction!")));
+        triggerManager.addTrigger(new Trigger(condition, new DebugResponse(
+                "You are lookin the the positive x and y direction!")));
 
     }
 }
